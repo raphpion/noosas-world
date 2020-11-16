43a53a30206f9c0b6de4ae5116f8a6fdec9641ce
Raphaël Pion <raph.pion@gmail.com>
2020-11-16 00:26:18 -0500

    moved and renamed tutorial screen

--- src/menus/tutorialScreen.js
+++ src/menus/tutorialScreen.js
@@ -1,21 +1,21 @@
-import { game } from './game.js'
-import { gameScreen, ctx, GAME_WIDTH, clearScreen, getScene } from './gameScreen.js'
-import { pixelClouds } from './backgrounds/pixelClouds.js'
-import { playMusic } from './gameAudio.js'
-import { player } from './player.js'
-import { tutorialToggleButton } from './buttons/tutorialToggleButton.js'
-import { isMouseOverButton } from './methods.js'
+import { game } from '../game.js'
+import { gameScreen, ctx, GAME_WIDTH, clearScreen, getScene } from '../gameScreen.js'
+import { pixelClouds } from '../backgrounds/pixelClouds.js'
+import { playMusic } from '../gameAudio.js'
+import { player } from '../player.js'
+import { tutorialToggleButton } from '../buttons/tutorialToggleButton.js'
+import { isMouseOverButton } from '../methods.js'
 
-const tutorial = {
+const tutorialScreen = {
   background: new Image(),
   pos: {
     x: 205,
     y: 84,
   },
   clear: () => {
-    document.removeEventListener('keydown', tutorial.keyDown)
-    document.removeEventListener('mousemove', tutorial.mouseMove)
-    document.removeEventListener('click', tutorial.mouseClick)
+    document.removeEventListener('keydown', tutorialScreen.keyDown)
+    document.removeEventListener('mousemove', tutorialScreen.mouseMove)
+    document.removeEventListener('click', tutorialScreen.mouseClick)
     pixelClouds.stop()
   },
   aKey: {
@@ -65,15 +65,15 @@ const tutorial = {
     pixelClouds.draw()
     ctx.drawImage(game.ground.img, game.ground.pos.x, game.ground.pos.y)
     player.draw()
-    ctx.drawImage(tutorial.background, tutorial.pos.x, tutorial.pos.y)
+    ctx.drawImage(tutorialScreen.background, tutorialScreen.pos.x, tutorialScreen.pos.y)
     ctx.fillStyle = 'black'
     ctx.textAlign = 'center'
     ctx.textBaseline = 'bottom'
-    ctx.drawImage(tutorial.aKey.img, tutorial.aKey.pos.x, tutorial.aKey.pos.y)
-    ctx.drawImage(tutorial.dKey.img, tutorial.dKey.pos.x, tutorial.dKey.pos.y)
-    ctx.drawImage(tutorial.enterKey.img, tutorial.enterKey.pos.x, tutorial.enterKey.pos.y)
-    ctx.drawImage(tutorial.escKey.img, tutorial.escKey.pos.x, tutorial.escKey.pos.y)
-    ctx.drawImage(tutorial.spacebarKey.img, tutorial.spacebarKey.pos.x, tutorial.spacebarKey.pos.y)
+    ctx.drawImage(tutorialScreen.aKey.img, tutorialScreen.aKey.pos.x, tutorialScreen.aKey.pos.y)
+    ctx.drawImage(tutorialScreen.dKey.img, tutorialScreen.dKey.pos.x, tutorialScreen.dKey.pos.y)
+    ctx.drawImage(tutorialScreen.enterKey.img, tutorialScreen.enterKey.pos.x, tutorialScreen.enterKey.pos.y)
+    ctx.drawImage(tutorialScreen.escKey.img, tutorialScreen.escKey.pos.x, tutorialScreen.escKey.pos.y)
+    ctx.drawImage(tutorialScreen.spacebarKey.img, tutorialScreen.spacebarKey.pos.x, tutorialScreen.spacebarKey.pos.y)
     tutorialToggleButton.draw()
     ctx.font = '24pt VT323'
     ctx.fillText('Ramassez des croquettes et', GAME_WIDTH / 2, 138)
@@ -89,20 +89,20 @@ const tutorial = {
     gameScreen.style.backgroundColor = game.background
     game.ground.img.src = '../assets/tiles/ground_grass.png'
     player.sprite.img.src = '../assets/sprites/noosa.png'
-    tutorial.background.src = '../assets/menu/tutorial_prompt.png'
-    tutorial.aKey.img.src = '../assets/menu/a_key.png'
-    tutorial.enterKey.img.src = '../assets/menu/enter_key.png'
-    tutorial.escKey.img.src = '../assets/menu/esc_key.png'
-    tutorial.dKey.img.src = '../assets/menu/d_key.png'
-    tutorial.shiftKey.img.src = '../assets/menu/shift_key.png'
-    tutorial.spacebarKey.img.src = '../assets/menu/spacebar_key.png'
+    tutorialScreen.background.src = '../assets/menu/tutorial_prompt.png'
+    tutorialScreen.aKey.img.src = '../assets/menu/a_key.png'
+    tutorialScreen.enterKey.img.src = '../assets/menu/enter_key.png'
+    tutorialScreen.escKey.img.src = '../assets/menu/esc_key.png'
+    tutorialScreen.dKey.img.src = '../assets/menu/d_key.png'
+    tutorialScreen.shiftKey.img.src = '../assets/menu/shift_key.png'
+    tutorialScreen.spacebarKey.img.src = '../assets/menu/spacebar_key.png'
     tutorialToggleButton.pos.x = 275
     tutorialToggleButton.pos.y = 456
     playMusic('arcade')
-    document.addEventListener('keydown', tutorial.keyDown)
-    document.addEventListener('mousemove', tutorial.mouseMove)
-    document.addEventListener('click', tutorial.mouseClick)
-    return setInterval(tutorial.draw, 1000 / 60)
+    document.addEventListener('keydown', tutorialScreen.keyDown)
+    document.addEventListener('mousemove', tutorialScreen.mouseMove)
+    document.addEventListener('click', tutorialScreen.mouseClick)
+    return setInterval(tutorialScreen.draw, 1000 / 60)
   },
   keyDown: e => {
     if (e.keyCode == 13) getScene(game)
@@ -120,4 +120,4 @@ const tutorial = {
   },
 }
 
-export { tutorial }
+export { tutorialScreen }
