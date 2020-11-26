61b3b99f5fe2fb1a5a9c4c3fd0e6452d765affd4
unknown <raph.pion@gmail.com>
2020-11-26 00:05:09 -0500

    Created Kibble score handling

--- src/prefabs/Kibble.js
+++ src/prefabs/Kibble.js
@@ -1,8 +1,11 @@
 import { ctx } from '../gameScreen.js'
+import { game } from '../game.js'
+import { GAME_WIDTH } from '../gameScreen.js'
+import { playSound } from '../gameAudio.js'
 
 // Classe d'objet d'une croquette
 class Kibble {
-  constructor(x, y) {
+  constructor(x = 0, y = 0) {
     this.sprite = {
       img: new Image(),
       index: 0,
@@ -24,21 +27,38 @@ class Kibble {
 
     this.updateHitbox()
   }
+  collide() {
+    // fonction qui s'exécute lorsque le joueur entre en collision avec la croquette
+    playSound('coin')
+
+    //* Test: on déplace la croquette en X
+    let newPosition
+
+    do {
+      // boucle pour empêcher que la croquette ne réapparaisse trop près de l'ancienne
+      newPosition = Math.floor(Math.random() * (GAME_WIDTH - 50))
+    } while (newPosition <= this.pos.x + 200 && newPosition >= this.pos.x - 200)
+
+    this.pos.x = newPosition
+
+    // augmentation du score
+    game.kibbles++
+  }
   draw() {
     // fonction pour dessiner la croquette à l'écran
-
-    //? L'image ne s'affiche pas
-    ctx.drawImage(this.sprite.img, this.sourceX, 0, 32, 32, this.pos.x, this.pos.y, 32, 32)
+    ctx.drawImage(this.sprite.img, this.sprite.sourceX, 0, 32, 32, this.pos.x, this.pos.y, 32, 32)
 
     //* DEBUG: AFFICHAGE DE LA HITBOX
-    for (let i = 0; i < this.hitbox.length; i++) {
-      ctx.fillStyle = 'rgba(255, 0, 0, 0.5)'
-      ctx.fillRect(
-        this.hitbox[i].pos.x + this.pos.x,
-        this.hitbox[i].pos.y + this.pos.y,
-        this.hitbox[i].width,
-        this.hitbox[i].height
-      )
+    if (localStorage.getItem('debugMode') == 'true') {
+      for (let i = 0; i < this.hitbox.length; i++) {
+        ctx.fillStyle = 'rgba(255, 0, 0, 0.5)'
+        ctx.fillRect(
+          this.hitbox[i].pos.x + this.pos.x,
+          this.hitbox[i].pos.y + this.pos.y,
+          this.hitbox[i].width,
+          this.hitbox[i].height
+        )
+      }
     }
   }
   updateHitbox() {
@@ -49,33 +69,33 @@ class Kibble {
     // Index 0
     if (this.sprite.index == 0) {
       this.hitbox.push(
-        { type: 'rectangle', pos: { x: 8, y: 8 }, width: 16, height: 16 }, // partie centrale
-        { type: 'rectangle', pos: { x: 10, y: 0 }, width: 12, height: 32 }, // ligne verticale
-        { type: 'rectangle', pos: { x: 0, y: 10 }, width: 32, height: 12 } // ligne horizontale
+        { pos: { x: 8, y: 8 }, width: 16, height: 16 }, // partie centrale
+        { pos: { x: 10, y: 0 }, width: 12, height: 32 }, // ligne verticale
+        { pos: { x: 0, y: 10 }, width: 32, height: 12 } // ligne horizontale
       )
     }
 
     // Index 1 & 5
     if (this.sprite.index == 1 || this.sprite.index == 5) {
       this.hitbox.push(
-        { type: 'rectangle', pos: { x: 12, y: 0 }, width: 8, height: 32 }, // ligne verticale
-        { type: 'rectangle', pos: { x: 6, y: 10 }, width: 20, height: 12 } // ligne horizontale
+        { pos: { x: 12, y: 0 }, width: 8, height: 32 }, // ligne verticale
+        { pos: { x: 6, y: 10 }, width: 20, height: 12 } // ligne horizontale
       )
     }
 
     // Index 2 & 4
     if (this.sprite.index == 2 || this.sprite.index == 4) {
       this.hitbox.push(
-        { type: 'rectangle', pos: { x: 12, y: 0 }, width: 8, height: 32 }, // ligne verticale
-        { type: 'rectangle', pos: { x: 8, y: 10 }, width: 16, height: 12 } // ligne horizontale
+        { pos: { x: 12, y: 0 }, width: 8, height: 32 }, // ligne verticale
+        { pos: { x: 8, y: 10 }, width: 16, height: 12 } // ligne horizontale
       )
     }
 
     // Index 3
     if (this.sprite.index == 3) {
       this.hitbox.push(
-        { type: 'rectangle', pos: { x: 14, y: 0 }, width: 4, height: 32 }, // ligne verticale
-        { type: 'rectangle', pos: { x: 12, y: 12 }, width: 8, height: 8 } // ligne horizontale
+        { pos: { x: 14, y: 0 }, width: 4, height: 32 }, // ligne verticale
+        { pos: { x: 12, y: 12 }, width: 8, height: 8 } // ligne horizontale
       )
     }
   }
