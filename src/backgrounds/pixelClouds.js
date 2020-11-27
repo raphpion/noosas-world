2f16e3437e43f0cec26c1d9cb6b1fcac7f696736
unknown <raph.pion@gmail.com>
2020-11-27 14:41:27 -0500

    Updated pixelClouds background object name

--- src/backgrounds/pixelClouds.js
+++ src/backgrounds/pixelClouds.js
@@ -1,36 +1,39 @@
-import { ctx, GAME_WIDTH } from '../gameScreen.js'
-
-// variable pour stocker l'intervalle de mouvement des nuages
-let cloudMoveInterval
+import { ctx, GAME_WIDTH, GAME_HEIGHT } from '../gameScreen.js'
+import { game } from '../game.js'
 
 // arrière-plan de nuages en jeu
-const pixelClouds = {
+const bg_pixelClouds = {
   img: new Image(),
   pos: {
     x: 0,
     y: 0,
   },
   draw: () => {
-    // fonction d'affichage de l'arrière-plan
-    ctx.drawImage(pixelClouds.img, pixelClouds.pos.x, pixelClouds.pos.y)
-    ctx.drawImage(pixelClouds.img, pixelClouds.pos.x + GAME_WIDTH, pixelClouds.pos.y)
-  },
-  move: (speed = 1) => {
-    // fonction d'initialisation du déplacement des nuages
-    cloudMoveInterval = setInterval(function () {
-      pixelClouds.pos.x--
+    // fonction d'affichage de l'arrière-plan de nuages
+    // on détermine combien de fois on doit afficher l'image horizontalement et verticalement
+    let repeatX = Math.ceil(game.map.width / GAME_WIDTH)
+    let repeatY = Math.ceil(game.map.height / GAME_HEIGHT)
 
-      // si le centre de l'image dépasse l'origine du canevas, on la replace à sa position initiale
-      if (pixelClouds.pos.x < -GAME_WIDTH) pixelClouds.pos.x = 0
-    }, 1000 / (30 * speed))
-  },
-  stop: () => {
-    // fonction d'arrêt du mouvement des nuages
-    clearInterval(cloudMoveInterval)
+    // on affiche à répétition l'image horizontalement avec la position relative à la map
+    // pour créer l'effet que les nuages sont plus loin, on soustrait le dixième de l'offset de la map à la position
+    for (let i = 0; i < repeatX; i++)
+      ctx.drawImage(
+        bg_pixelClouds.img,
+        bg_pixelClouds.pos.x + i * GAME_WIDTH - Math.floor(game.map.offset.x / 10),
+        bg_pixelClouds.pos.y - Math.floor(game.map.offset.y / 10)
+      )
+    // on affiche à répétition l'image verticalement avec la position relative à la map
+    // pour créer l'effet que les nuages sont plus loin, on soustrait le dixième de l'offset de la map à la position
+    for (let i = 0; i < repeatY; i++)
+      ctx.drawImage(
+        bg_pixelClouds.img,
+        bg_pixelClouds.pos.x - Math.floor(game.map.offset.x / 10),
+        bg_pixelClouds.pos.y + i * Math.floor(GAME_HEIGHT - game.map.offset.y / 10)
+      )
   },
 }
 
 // on charge l'image des nuages
-pixelClouds.img.src = '../assets/backgrounds/pixel_clouds.png'
+bg_pixelClouds.img.src = '../assets/backgrounds/pixel_clouds.png'
 
-export { pixelClouds }
+export { bg_pixelClouds }
